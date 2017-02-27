538b23ddef4df2a45e48643005a2397116346f95
Mehmet Aktaş <mehmetaktas999@gmail.com>
2017-02-27 11:20:30 +0300

    commit

--- SimpleBlog2/SimpleBlog2/App_Start/RouteConfig.cs
+++ SimpleBlog2/SimpleBlog2/App_Start/RouteConfig.cs
@@ -13,11 +13,9 @@ namespace SimpleBlog2
         {
             routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
 
-            routes.MapRoute(
-                name: "Default",
-                url: "{controller}/{action}/{id}",
-                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
-            );
+            routes.MapRoute("Home", "", new { Controller = "Posts", Action = "Index" });
+
+            routes.MapRoute("Login", "login", new { Controller = "Auth", Action = "Login" });
         }
     }
 }
